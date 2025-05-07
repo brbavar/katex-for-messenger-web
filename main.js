2662c58476b429ce209bc4751ac45e60fcbad7c9
Ben Bavar <ben.bavar@gmail.com>
2025-05-07 19:15:26 -0400

    Make every value assigned to newContainer a span, rather than making some spans and others divs; get inline tex to display properly, without a bunch of space separating it from the surrounding text

--- main.js
+++ main.js
@@ -52,37 +52,78 @@ const getTexBounds = (openingDelim, txt) => {
   return [];
 };
 
-const preserveNonTex = (txt, precedesTex, div, renderType) => {
+const preserveNonTex = (txt, precedesTex, container, renderType) => {
   if (txt !== '') {
-    const newDiv = document.createElement('div');
-    newDiv.classList.add(`${precedesTex ? 'pre' : 'post'}-tex`);
-    newDiv.textContent = txt;
-    if (precedesTex) div.insertBefore(newDiv, div.children[0]);
-    else {
-      div.appendChild(newDiv);
+    // const newContainer = document.createElement(renderType ? 'div' : 'span');
+    const newContainer = document.createElement('span');
+    newContainer.classList.add(`${precedesTex ? 'pre' : 'post'}-tex`);
+    newContainer.textContent = txt;
+    if (precedesTex) {
+      // const newContainer = document.createElement(renderType ? 'div' : 'span');
+      // newContainer.classList.add('pre-tex');
+      // newContainer.textContent = txt;
+
+      container.insertBefore(newContainer, container.children[0]);
+
+      let bounds = [[], []];
+      let otherRenderType = renderType === 1 ? 0 : 1;
+      bounds[otherRenderType].push(
+        ...getTexBounds(otherRenderType ? '$$' : '\\(', txt)
+      );
+      if (bounds[otherRenderType].length > 0) {
+        console.log('otherRenderType: ' + otherRenderType);
+        renderTex(otherRenderType, newContainer, bounds);
+      }
+    } else {
+      // const newContainer = document.createElement(
+      //   container.classList.contains('pre-tex') ? 'span' : 'div'
+      // );
+      // newContainer.classList.add('post-tex');
+      // newContainer.textContent = txt;
+
+      container.appendChild(newContainer);
 
       let bounds = [[], []];
       bounds[renderType].push(...getTexBounds(renderType ? '$$' : '\\(', txt));
       if (bounds[renderType].length > 0) {
-        renderTex(renderType, newDiv, bounds);
+        renderTex(renderType, newContainer, bounds);
       }
     }
   }
 };
 
-const renderTex = (renderType, msg, texBounds) => {
-  const bounds = texBounds[renderType];
+const renderTex = (renderType, container /*, texBounds*/) => {
+  // const bounds = texBounds[renderType];
+  const bounds = getTexBounds(renderType ? '$$' : '\\(', container.textContent);
   if (bounds.length > 1) {
-    const origTxt = msg.textContent;
+    const origTxt = container.textContent;
     const preTex = origTxt.substring(0, bounds[0]);
+    const tex = origTxt.substring(bounds[0] + 2, bounds[1]);
     const postTex = origTxt.substring(bounds[1] + 2);
 
-    katex.render(origTxt.substring(bounds[0] + 2, bounds[1]), msg, {
+    // if (renderType === 0)
+    console.log(
+      'bounds: ' +
+        bounds +
+        '\n\npreTex: ' +
+        preTex +
+        '\n\ntex: ' +
+        tex +
+        '\n\npostTex: ' +
+        postTex
+    );
+
+    // Add code to keep track of where bounds are as new divs/spans are introduced
+
+    katex.render(tex, container, {
       displayMode: renderType,
     });
 
-    preserveNonTex(preTex, true, msg, renderType);
-    preserveNonTex(postTex, false, msg, renderType);
+    // return; // Test 1
+
+    preserveNonTex(preTex, true, container, renderType);
+    // return; // Test 2
+    preserveNonTex(postTex, false, container, renderType);
   }
 };
 
@@ -109,6 +150,9 @@ const childListObserver = new MutationObserver((mutations) => {
           texBounds[0].push(...getTexBounds('\\(', txt));
           texBounds[1].push(...getTexBounds('$$', txt));
 
+          if (msg.textContent.startsWith('This is related'))
+            console.log('texBounds[0]: ' + texBounds[0]);
+
           const inlineTexFound = texBounds[0].length > 0;
           const displayTexFound = texBounds[1].length > 0;
 
@@ -123,8 +167,11 @@ const childListObserver = new MutationObserver((mutations) => {
             parent.style.border =
               '14px solid var(--mwp-message-row-background)';
 
+            // Don't render directly into msg in both invocations below
             renderTex(0, msg, texBounds);
+            // return; // Test 3
             renderTex(1, msg, texBounds);
+            return; // Tests 1, 2, 4
           }
         });
       }
