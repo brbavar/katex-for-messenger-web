965ddd59d0d2889838e419835d6d16234445a764
Ben Bavar <ben.bavar@gmail.com>
2025-05-08 22:38:52 -0400

    Only access and modify textContent in for loop. If you access or modify innerHTML instead, symbols such as > and < might be replaced with character entities, and some occurrences of \ may go missing, leading to misplacement of span tags. Save modification of msg.innerHTML for immediately after the loop, when you have your full HTML string ready for conversion into HTML. After that you can properly render the TeX meant to be contained in the spans. Also, you no longer need text color, padding, or border styles.

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ const getTexBounds = (msg) => {
   const txt = msg.textContent;
   const bounds = [];
 
+  console.log(`txt = ${txt}`);
+
   const delimAt = (i) => {
     return (
       (txt[i] === '$' && txt[i + 1] === '$') ||
@@ -36,25 +38,52 @@ const getTexBounds = (msg) => {
   let l = 0,
     r = 0;
   while (l < txt.length) {
-    if (openingDelimAt(l)) {
+    if (l > 1664 && l < 1671) {
+      console.log(`l before = ${l}, txt[l] = ${l < txt.length ? txt[l] : ''}`);
+    }
+    if (
+      openingDelimAt(l) &&
+      (bounds.length === 0 || l !== bounds[bounds.length - 1][1])
+    ) {
+      if (l > 1664 && l < 1671) console.log(`opening delim detected at ${l}`);
       r = l + 2;
 
       // Warn user that a sequence of symbols visually indistinguishable from a(n opening)
       // delimiter will be interpreted as one unless characters are escaped. Actually, MAYBE NO NEED
       while (r + 1 < txt.length && !(closingDelimAt(r) && txt[l] == txt[r])) {
+        if (l > 1664 && l < 1671) {
+          console.log(`r = ${r}, txt[r] = ${r < txt.length ? txt[r] : ''}`);
+          console.log(`no matching closing delim detected at ${r}`);
+        }
         if (openingDelimAt(r) && txt[l] == txt[r]) {
+          if (l > 1664 && l < 1671)
+            console.log(`but identical opening delim was detected at ${r}`);
           l = r;
           r += 2;
         } else {
+          if (l > 1664 && l < 1671)
+            console.log(`no identical opening delim at ${r} either`);
           r++;
         }
       }
 
       if (closingDelimAt(r) && txt[l] == txt[r]) {
+        console.log(
+          `bounds[bounds.length - 1][0] = ${
+            bounds.length > 0 ? bounds[bounds.length - 1][1] : ''
+          }`
+        );
+        console.log(
+          `matching closing delim ${txt[r]}${txt[r + 1]} detected at ${r}`
+        );
+        console.log(`putting [${l}, ${r}] in bounds array`);
         bounds.push([l, r]);
+        // return;
       }
     }
     l++;
+    if (l > 1665 && l < 1672)
+      console.log(`l after = ${l}, txt[l] = ${l < txt.length ? txt[l] : ''}`);
   }
 
   return bounds;
@@ -85,41 +114,84 @@ const childListObserver = new MutationObserver((mutations) => {
             texBounds = getTexBounds(msg);
           }
 
-          for (let i = 0; i < texBounds.length; i++) {
-            const offset = 32 * i;
-            console.log(
-              `i = ${i}\noffset = ${offset}\ntexBounds[i][0] = ${
-                texBounds[i][0]
-              }\ntexBounds[i][1] = ${
-                texBounds[i][1]
-              }\npreTex = ${msg.innerHTML.substring(
+          if (texBounds !== undefined) {
+            for (let i = 0; i < texBounds.length; i++) {
+              const offset = 32 * i;
+              // console.log(
+              //   `i = ${i}\noffset = ${offset}\ntexBounds[i][0] = ${
+              //     texBounds[i][0]
+              //   }\ntexBounds[i][1] = ${
+              //     texBounds[i][1]
+              //   }\npreTex = ${msg.innerHTML.substring(
+              //     0,
+              //     texBounds[i][0] + offset
+              //   )}\ntex = ${msg.innerHTML.substring(
+              //     texBounds[i][0] + offset,
+              //     texBounds[i][1] + 2 + offset
+              //   )}\npostTex = ${msg.innerHTML.substring(
+              //     texBounds[i][1] + 2 + offset
+              //   )}`
+              // );
+
+              // const newHTML = `${msg.innerHTML.substring(
+              //   0,
+              //   texBounds[i][0] + offset
+              // )}<span class='renderable'>${msg.innerHTML.substring(
+              //   texBounds[i][0] + offset,
+              //   texBounds[i][1] + 2 + offset
+              // )}</span>${msg.innerHTML.substring(
+              //   texBounds[i][1] + 2 + offset
+              // )}`;
+              // console.log(
+              //   `opening delim is ${msg.innerHTML[texBounds[i][0] + offset]}${
+              //     msg.innerHTML[texBounds[i][0] + offset + 1]
+              //   }`
+              // );
+              // console.log(
+              //   `closing delim is ${msg.innerHTML[texBounds[i][1] + offset]}${
+              //     msg.innerHTML[texBounds[i][1] + offset + 1]
+              //   }`
+              // );
+
+              // console.log(`newHTML.length = ${newHTML.length}`);
+
+              msg.textContent = `${msg.textContent.substring(
                 0,
                 texBounds[i][0] + offset
-              )}\ntex = ${msg.innerHTML.substring(
+              )}<span class='renderable'>${msg.textContent.substring(
                 texBounds[i][0] + offset,
                 texBounds[i][1] + 2 + offset
-              )}\npostTex = ${msg.innerHTML.substring(
+              )}</span>${msg.textContent.substring(
                 texBounds[i][1] + 2 + offset
-              )}`
-            );
+              )}`;
+              // console.log(`msg.innerHTML.length = ${msg.innerHTML.length}`);
 
-            msg.innerHTML = `${msg.innerHTML.substring(
-              0,
-              texBounds[i][0] + offset
-            )}<span class='renderable'>${msg.innerHTML.substring(
-              texBounds[i][0] + offset,
-              texBounds[i][1] + 2 + offset
-            )}</span>${msg.innerHTML.substring(texBounds[i][1] + 2 + offset)}`;
+              // if (i === 0) return;
+
+              // if (i === 1) return;
+
+              // if (i === 2) return;
+            }
+
+            msg.innerHTML = msg.textContent;
+
+            msg.querySelectorAll('span.renderable').forEach((span) => {
+              console.log(`span.textContent = ${span.textContent}`);
+              console.log(
+                `span.textContent.substring(2, span.innerHTML.length - 2) = ${span.textContent.substring(
+                  2,
+                  span.textContent.length - 2
+                )}`
+              );
+              katex.render(
+                span.textContent.substring(2, span.textContent.length - 2),
+                span,
+                {
+                  displayMode: span.textContent[0] === '$',
+                }
+              );
+            });
           }
-
-          let parent = bubble.parentNode;
-
-          while (parent != null) {
-            if (parent.role === 'row') break;
-            parent = parent.parentNode;
-          }
-
-          parent.style.border = '14px solid var(--mwp-message-row-background)';
         });
       }
     });
