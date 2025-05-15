93697f96d884a9c01734a3a5dbcce073259ebd2f
Ben Bavar <ben.bavar@gmail.com>
2025-05-14 22:42:15 -0400

    Render Tex properly when a chat is opened in Messenger via the menu ("Chat tab settings") built into each chat box. Had this menu item/button been designed to trigger a page reload, the extension would have already rendered the notation as desired. But it does not trigger one.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-let chatBubbleObserver;
 class DomInfo {
   #chat = null;
-  #messengerButton = null;
   #chatBoxContainer = null;
   #chatBoxContainerObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
@@ -12,19 +10,63 @@ class DomInfo {
         if (messageGrid !== null) {
           if (!this.#chatBoxes.includes(node)) {
             this.#chatBoxes.push(node);
-            handleOpenChat(node, messageGrid);
+            handleChat(node, messageGrid);
           }
+          // const chatSettingsButton = node.querySelector(
+          //   'div[aria-label="Chat settings"][role="button"].x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.xe8uvvx.xdj266r.xat24cr.x1mh8g0r.x2lwn1j.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x3nfvp2.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.xs83m0k.x1emribx.xeuugli'
+          // );
+          // if (!this.#chatSettingsButtons.includes(chatSettingsButton)) {
+          //   this.#chatSettingsButtons.push(chatSettingsButton);
+
+          //   chatSettingsButton.addEventListener('click', () => {
+          //     setTimeout(() => {
+          //       console.log(
+          //         `delaying queries and addition of event listener for 10 ms`
+          //       );
+          //       const chatMenu = document.querySelector(
+          //         'div[aria-label="Chat tab settings"][role="menu"].x1n2onr6.x1jqylkn.xe5xk9h'
+          //       );
+          //       const openInMessengerButton = chatMenu.querySelector(
+          //         '[href^="/messages"][role="menuitem"].x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.xe8uvvx.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x87ps6o.x1lku1pv.x1a2a7pz.xjyslct.x9f619.x1ypdohk.x78zum5.x1q0g3np.x2lah0s.x1i6fsjq.xfvfia3.xnqzcj9.x1gh759c.x1n2onr6.x16tdsg8.x1ja2u2z.x6s0dn4.x1y1aw1k.xwib8y2.x1q8cg2c.xnjli0'
+          //       );
+          //       openInMessengerButton.addEventListener(
+          //         'click',
+          //         () => {
+          //           console.log(`delaying startUp for 10 ms`);
+          //           setTimeout(startUp, 10);
+          //         } /* () => {
+          //                 this.reset();
+          //                 this.setChat();
+          //               } */
+          //       );
+          //     }, 10);
+          //   });
+          // }
         }
       });
       mutation.removedNodes.forEach((node) => {
-        const i = this.#chatBoxes.indexOf(node);
+        let i = this.#chatBoxes.indexOf(node);
         if (i !== -1) {
           this.#chatBoxes.splice(i, 1);
+          this.#chatSettingsButtons.splice(i, 1);
         }
       });
     });
   });
   #chatBoxes = [];
+  #chatSettingsButtons = [];
+  #chatSettingsButton = null;
+  #chatMenu = null;
+  #openInMessengerButton = null;
+
+  // reset() {
+  //   [this.#chat, this.#chatBoxContainer, this.#chatSettingsButton, this.#chatMenu, this.#openInMessengerButton].forEach((el) => {
+  //     el = null;
+  //   });
+  //   [(this.#chatBoxes, this.#chatSettingsButtons)].forEach((arr) => {
+  //     arr.length = 0;
+  //   });
+  // }
 
   getChat() {
     return this.#chat;
@@ -39,17 +81,6 @@ class DomInfo {
     if (arguments.length === 1) {
       this.#chat = chatBox;
     }
-    console.log(`chat has aria label ${this.#chat.getAttribute('aria-label')}`);
-  }
-
-  getMessengerButton() {
-    return this.#messengerButton;
-  }
-
-  setMessengerButton() {
-    this.#messengerButton = document.querySelector(
-      'div[aria-label="Messenger"][role="button"].x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.xzolkzo.x12go9s9.x1rnf11y.xprq8jg.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x78zum5.xl56j7k.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1vqgdyp.x100vrsf.x1qhmfi1'
-    );
   }
 
   getChatBoxContainer() {
@@ -75,9 +106,106 @@ class DomInfo {
   }
 
   setChatBoxes() {
+    this.#chatBoxes.length = 0;
+    console.log(`${this.#chatBoxContainer.children.length} chatBoxes found`);
     for (const chatBox of this.#chatBoxContainer.children) {
       this.#chatBoxes.push(chatBox);
+
+      this.setChatSettingsButton(chatBox);
+      console.log(
+        `chatSettingsButton starts off with a value of ${
+          this.#chatSettingsButton
+        }`
+      );
+      if (this.#chatSettingsButton === null) {
+        // console.log(`chatSettingsButton starts off null`);
+
+        setTimeout(() => {
+          this.setChatSettingsButton(chatBox);
+          console.log(
+            `But after 300 ms chatSettingsButton is ${this.#chatSettingsButton}`
+          ); // sometimes still null after 200 ms
+
+          this.handleChatSettingsButtons();
+        }, 300);
+      } else {
+        this.handleChatSettingsButtons();
+      }
     }
+  }
+
+  getChatSettingsButtons() {
+    return this.#chatSettingsButtons;
+  }
+
+  setChatSettingsButtons() {
+    this.#chatSettingsButtons.length = 0;
+    for (const chatBox of this.#chatBoxes) {
+      this.#chatSettingsButtons.push(
+        chatBox.querySelector(
+          'div[aria-label="Chat settings"][role="button"].x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.xe8uvvx.xdj266r.xat24cr.x1mh8g0r.x2lwn1j.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x3nfvp2.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.xs83m0k.x1emribx.xeuugli'
+        )
+      );
+    }
+  }
+
+  handleChatSettingsButtons() {
+    this.#chatSettingsButtons.push(this.#chatSettingsButton);
+
+    this.#chatSettingsButton.addEventListener('click', () => {
+      this.setChatMenu();
+      if (this.#chatMenu === null) {
+        console.log(`chatMenu starts off null`);
+        setTimeout(() => {
+          this.setChatMenu();
+          console.log(`But after 50 ms chatMenu is ${this.#chatMenu}`);
+          this.setOpenInMessengerButton();
+
+          this.#openInMessengerButton.addEventListener('click', () => {
+            setTimeout(startUp, 50);
+          });
+        }, 50);
+      } else {
+        console.log(
+          `chatMenu starts off not null but with a value of ${this.#chatMenu}`
+        );
+        this.setOpenInMessengerButton();
+
+        this.#openInMessengerButton.addEventListener('click', () => {
+          setTimeout(startUp, 10);
+        });
+      }
+    });
+  }
+
+  getChatSettingsButton() {
+    return this.#chatSettingsButton;
+  }
+
+  setChatSettingsButton(chatBox) {
+    this.#chatSettingsButton = chatBox.querySelector(
+      'div[aria-label="Chat settings"][role="button"].x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.xe8uvvx.xdj266r.xat24cr.x1mh8g0r.x2lwn1j.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x3nfvp2.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.xs83m0k.x1emribx.xeuugli'
+    );
+  }
+
+  getChatMenu() {
+    return this.#chatMenu;
+  }
+
+  setChatMenu() {
+    this.#chatMenu = document.querySelector(
+      'div[aria-label="Chat tab settings"][role="menu"].x1n2onr6.x1jqylkn.xe5xk9h'
+    );
+  }
+
+  getOpenInMessengerButton() {
+    return this.#openInMessengerButton;
+  }
+
+  setOpenInMessengerButton() {
+    this.#openInMessengerButton = this.#chatMenu.querySelector(
+      '[href^="/messages"][role="menuitem"].x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.xe8uvvx.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x87ps6o.x1lku1pv.x1a2a7pz.xjyslct.x9f619.x1ypdohk.x78zum5.x1q0g3np.x2lah0s.x1i6fsjq.xfvfia3.xnqzcj9.x1gh759c.x1n2onr6.x16tdsg8.x1ja2u2z.x6s0dn4.x1y1aw1k.xwib8y2.x1q8cg2c.xnjli0'
+    );
   }
 }
 
@@ -263,20 +391,26 @@ const sendStatusObserver = new MutationObserver((mutations) => {
   });
 });
 
+let chatBubbleObserver;
+
+const handleChatBubbles = (node) => {
+  if ('querySelectorAll' in node) {
+    const yourChatBubbles = node.querySelectorAll(
+      '.html-div.x11i5rnm.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.xat24cr.xdj266r.xeuugli.x1vjfegm'
+    );
+    const theirChatBubbles = node.querySelectorAll(
+      '.html-div.x11i5rnm.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1gslohp.x12nagc.x1yc453h.x126k92a.x18lvrbx'
+    );
+    [...yourChatBubbles, ...theirChatBubbles].forEach(parseContent);
+  }
+};
+
 const handleMessageGrid = (grid) => {
+  handleChatBubbles(grid);
+
   chatBubbleObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-      mutation.addedNodes.forEach((node) => {
-        if ('querySelectorAll' in node) {
-          const yourChatBubbles = node.querySelectorAll(
-            '.html-div.x11i5rnm.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.xat24cr.xdj266r.xeuugli.x1vjfegm'
-          );
-          const theirChatBubbles = node.querySelectorAll(
-            '.html-div.x11i5rnm.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1gslohp.x12nagc.x1yc453h.x126k92a.x18lvrbx'
-          );
-          [...yourChatBubbles, ...theirChatBubbles].forEach(parseContent);
-        }
-      });
+      mutation.addedNodes.forEach(handleChatBubbles);
     });
   });
 
@@ -324,12 +458,12 @@ const handleTextbox = (chat) => {
     'div[aria-label="Message"][role="textbox"][contenteditable="true"][data-lexical-editor="true"].xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.notranslate'
   );
   if (textbox === null) {
-    console.log(`textbox starts off null`);
+    // console.log(`textbox starts off null`);
     setTimeout(() => {
       textbox = chat.querySelector(
         'div[aria-label="Message"][role="textbox"][contenteditable="true"][data-lexical-editor="true"].xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.notranslate'
       );
-      console.log(`But after delay textbox is ${textbox}`); // sometimes still null after 200 ms
+      // console.log(`But after delay textbox is ${textbox}`); // sometimes still null after 200 ms
       listenForNewMessages(chat, textbox);
     }, 500);
   } else {
@@ -345,18 +479,20 @@ const handleTextbox = (chat) => {
   //   console.log(`sendButton created`);
 };
 
-const handleOpenChat = (chat, messageGrid = null) => {
+const handleChat = (chat, messageGrid = null) => {
   if (messageGrid === null) {
+    console.log(`messageGrid starts off null`);
     setTimeout(() => {
       messageGrid = chat.querySelector(
         'div[aria-label^="Messages in conversation"][role="grid"].x1uipg7g.xu3j5b3.xol2nv.xlauuyb.x26u7qi.x19p7ews.x78zum5.xdt5ytf.x1iyjqo2.x6ikm8r.x10wlt62'
       );
+      console.log(`But after 250 ms messageGrid is ${messageGrid}`);
       handleMessageGrid(messageGrid);
 
       // Set up keydown event listener below, parse and render preexisting messages containing TeX code above
 
       handleTextbox(chat);
-    }, 200);
+    }, 250);
   } else {
     handleMessageGrid(messageGrid);
 
@@ -370,22 +506,25 @@ const handleChatBoxContainer = (domInfo) => {
   domInfo.setChatBoxes();
   if (domInfo.getChatBoxes() !== null) {
     for (const chat of domInfo.getChatBoxes()) {
-      handleOpenChat(chat);
+      handleChat(chat);
     }
+  } else {
+    console.log(`chatBoxes starts off null`);
   }
   domInfo.observeChatBoxContainer();
 };
 
-window.onload = () => {
+const startUp = () => {
   const domInfo = new DomInfo();
 
-  if (document.location.href.startsWith('https://www.facebook.com/messages')) {
+  if (window.location.href.startsWith('https://www.facebook.com/messages')) {
     domInfo.setChat();
     if (domInfo.getChat() === undefined || domInfo.getChat() === null) {
       console.log('chat starts off null');
       setTimeout(() => {
         domInfo.setChat();
-        handleOpenChat(domInfo.getChat());
+        console.log(`But after 200 ms chat is ${domInfo.getChat()}`);
+        handleChat(domInfo.getChat());
       }, 200);
     } else {
       console.log(
@@ -398,32 +537,78 @@ window.onload = () => {
           ).length
         } elements match query selector`
       );
-      handleOpenChat(domInfo.getChat());
+      handleChat(domInfo.getChat());
     }
   } else {
-    domInfo.setMessengerButton();
     domInfo.setChatBoxContainer();
 
-    console.log(
-      `chatBoxContainer starts off with value of ${domInfo.getChatBoxContainer()}`
-    );
+    // console.log(
+    //   `chatBoxContainer starts off with value of ${domInfo.getChatBoxContainer()}, containing ${
+    //     domInfo.getChatSettingsButtons().length
+    //   } chatSettingsButtons`
+    // );
 
     if (domInfo.getChatBoxContainer() === null) {
       setTimeout(() => {
         domInfo.setChatBoxContainer();
-        console.log(
-          `But after 200 ms chatBoxContainer is ${domInfo.getChatBoxContainer()}`
-        ); // sometimes still null after 100 ms
-        handleChatBoxContainer(domInfo);
+        // console.log(
+        //   `But after 200 ms chatBoxContainer is ${domInfo.getChatBoxContainer()}, containing ${
+        //     domInfo.getChatSettingsButtons().length
+        //   } chatSettingsButtons`
+        // ); // sometimes still null after 100 ms
+        if (domInfo.getChatSettingsButtons().length === 0) {
+          setTimeout(() => {
+            domInfo.setChatSettingsButtons();
+            // console.log(
+            //   `But after 200 ms chatBoxContainer is ${domInfo.getChatBoxContainer()}, containing ${
+            //     domInfo.getChatSettingsButtons().length
+            //   } chatSettingsButtons`
+            // );
+            handleChatBoxContainer(domInfo);
+          }, 200);
+        } else {
+          handleChatBoxContainer(domInfo);
+        }
       }, 200);
     } else {
-      handleChatBoxContainer(domInfo);
+      if (domInfo.getChatSettingsButtons().length === 0) {
+        setTimeout(() => {
+          domInfo.setChatSettingsButtons();
+          // console.log(
+          //   `But after 200 ms chatBoxContainer is ${domInfo.getChatBoxContainer()}, containing ${
+          //     domInfo.getChatSettingsButtons().length
+          //   } chatSettingsButtons`
+          // );
+          handleChatBoxContainer(domInfo);
+        }, 200);
+      } else {
+        handleChatBoxContainer(domInfo);
+      }
     }
   }
 };
 
-// const urlChangeObserver = new MutationObserver((mutations) => {
-//   mutations.forEach((mutation) => {});
-// });
+window.onload = startUp;
 
-// urlChangeObserver.observe();
+(() => {
+  const pushState = history.pushState;
+  history.pushState = () => {
+    pushState.apply(history /*, arguments */);
+    window.dispatchEvent(new Event('locationchange'));
+  };
+
+  const replaceState = history.replaceState;
+  history.replaceState = () => {
+    replaceState.apply(history /*, arguments */);
+    window.dispatchEvent(new Event('locationchange'));
+  };
+
+  window.addEventListener('popstate', () => {
+    window.dispatchEvent(new Event('locationchange'));
+  });
+
+  window.addEventListener('locationchange', () => {
+    console.log('URL changed to:', window.location.href);
+    startUp();
+  });
+})();
